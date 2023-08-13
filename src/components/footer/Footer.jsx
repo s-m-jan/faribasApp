9bc07463bf52cf0debfd0106ac0ff4530444f5ab
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-08-13 21:15:00 +0330

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,19 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="main-footer">
-        <div className="container"></div>
-        <div className="row">
-            {/* column1 */}
-            <div className="col-md-3 col-sm-6"></div>
-            <h4>Lorem, ipsum.</h4>
-            <ul className='list-unstyled'>
-                <li>Lorem ipsum.</li>
-                <li>Lorem ipsum.</li>
-                <li>Lorem ipsum.</li>
-                <li>Lorem ipsum.</li>
-            </ul>
-        </div>
+    <div>
+        <p>Footer</p>
     </div>
   )
 }
