d9bee97ab078176ead2a24e3a2b5ec99dc1e4175
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-08-20 20:37:30 +0330

    start to create the about us page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from './AboutUs.module.css'
+
 
 export default function AboutUs() {
   return (
-    <div>
-      about
-      
+    <div className= {`container-fluid text-center ${styled.aboutUs}`}>
+    <div className="row ">
+      <div className="col-lg-6">
+        Column
+      </div>
+      <div className="col-lg-6">
+        Column
+      </div>
     </div>
+  </div>
   )
 }
 
