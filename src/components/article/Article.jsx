cbe121d9272c07a1b60a70bdba511f020173784a
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-09-04 13:00:35 +0330

    Single Article Page finished

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,14 +1,28 @@
-
-
-
+import { useQuery } from "react-query";
+import Axios from "axios";
+import { useParams } from "react-router-dom";
+import style from "./Article.module.css";
 const Article = () => {
-    
-
+  const params = useParams();
+  const { data } = useQuery(["singleArticle"], () => {
+    return Axios.get(`http://localhost:8000/questions/${params.id}`).then(
+      (res) => res.data
+    );
+  });
   return (
-    <div>
-      <h1>single Article</h1>
-    </div>
-  )
-}
+    <div className={style.container}>
+      <h1>{data?.title}</h1>
+      <div className={style.title}>
+        <p>{data?.content}</p>
+        <div className={style.imgWrapper}>
 
-export default Article
+        <img src={data?.imageUrl} />
+        </div>
+      </div>
+
+      <p>{data?.body}</p>
+    </div>
+  );
+};
+
+export default Article;
